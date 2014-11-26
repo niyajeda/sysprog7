cedfccd944319c02393aa05f2cc581af84786451
daniel <daniel.schroeder7@yahoo.de>
2014-11-26 11:42:56 +0100

    Fehler wenn Server keine POST-Anfragen bearbeiten kann und sofort die Anfrage verweigert. Dann wird ein Unexpected-EOF Fehler geworfen. Noch keinen guten Fix gefunden.

---
+++ Aufgabe24/post.go
@@ -0,0 +1,37 @@
+package main
+
+import(
+	"fmt"
+	"os"
+	"io/ioutil"
+	"net/http"
+)
+
+func check(e error){
+	if e != nil{
+		panic(e)
+	}
+}
+
+func main(){
+	client := &http.Client{}
+	if len(os.Args) < 2{
+		fmt.Printf("usage: post <Portnumber>\n")
+	}else{
+		fmt.Printf("# Baue Verbindung Server auf\n")
+		req, err := http.NewRequest("POST","http://localhost:"+string(os.Args[1]), nil)
+		check(err)
+		req.Close = true
+		req.Header.Add("User-Agent", "My SysProg Client")
+
+		fmt.Printf("# Connected \n# Schicke POST request an Webserver\n")
+		resp, err := client.Do(req)
+		check(err)
+		res, err := ioutil.ReadAll(resp.Body)
+		check(err)
+		resp.Body.Close()
+		err = ioutil.WriteFile("response.html", res, 0644)
+		check(err)
+		fmt.Printf("%s %s\nDate: %s\nServer: %s\nX-Powered-By: %s\nLast-Modified: %s\nExpires: %s\nETag: %s\nPragma: %s\nCache-Control: %s\nVary: %s\nContent-Type: %s\n", resp.Proto, resp.Status, resp.Header.Get("Date"), resp.Header.Get("Server"), resp.Header.Get("X-Powered-By"), resp.Header.Get("Last-Modified"), resp.Header.Get("Expires"), resp.Header.Get("Etag"), resp.Header.Get("Pragma"), resp.Header.Get("Cache-Control"), resp.Header.Get("Vary"), resp.Header.Get("Content-Type"))
+	}
+}
